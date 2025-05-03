5faa5765fd417de3a7054650c06013ff7011c9f6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-03 14:16:47 +0200

    golden_bay

--- render.py
+++ render.py
@@ -478,18 +478,18 @@ def eval_brdf(data_root: str, scene: Scene, model_path: str, name: str) -> None:
         mse_loss += masked_diff.mean()
         # albedo_map[mask] *= three_channel_ratio
         # albedo_map[mask] *= single_channel_ratio
-        # albedo_map = albedo_map.permute(2, 0, 1)  # [3, H, W]
-        # albedo_gt = albedo_gt.permute(2, 0, 1)  # [3, H, W]
+        albedo_map = albedo_map.permute(2, 0, 1)  # [3, H, W]
+        albedo_gt = albedo_gt.permute(2, 0, 1)  # [3, H, W]
         # torchvision.utils.save_image(albedo_map, os.path.join(pbr_path, f"{idx:05d}_albedo.png"))
         # torchvision.utils.save_image(albedo_gt, os.path.join(pbr_path, f"{idx:05d}_albedo_gt.png"))
-        # albedo_psnr_avg += get_psnr(albedo_gt, albedo_map).mean().double()
-        # albedo_ssim_avg += get_ssim(albedo_gt, albedo_map).mean().double()
-        # albedo_lpips_avg += lpips_fn(albedo_gt, albedo_map).mean().double()
+        albedo_psnr_avg += get_psnr(albedo_gt, albedo_map).mean().double()
+        albedo_ssim_avg += get_ssim(albedo_gt, albedo_map).mean().double()
+        albedo_lpips_avg += lpips_fn(albedo_gt, albedo_map).mean().double()
         # roughmse = mse(albedo_gt, albedo_map).double()
 
-    # albedo_psnr = albedo_psnr_avg / len(frames)
-    # albedo_ssim = albedo_ssim_avg / len(frames)
-    # albedo_lpips = albedo_lpips_avg / len(frames)
+    albedo_psnr = albedo_psnr_avg / len(frames)
+    albedo_ssim = albedo_ssim_avg / len(frames)
+    albedo_lpips = albedo_lpips_avg / len(frames)
     roughmse = mse_loss / len(frames)
     print(f"roughmse: {roughmse}")
 
