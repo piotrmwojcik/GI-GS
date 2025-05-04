99615fd86ce21eba027661467a4a394b430b39a9
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-05 01:11:40 +0200

    small envmap

--- pbr/light.py
+++ pbr/light.py
@@ -121,7 +121,7 @@ class CubemapLight(nn.Module):
         H = envmap_train.shape[2]  # Get height, [1, C, H, W]
         H_half = H // 2  # Midpoint for splitting
 
-        #print('!!! ', envmap_train.shape, self.gt_envmap.shape)
+        print('!!! ', envmap_train.shape, self.gt_envmap.shape)
 
         upper_loss = F.mse_loss(envmap_train[:, :H_half, :], self.gt_envmap[:, :H_half, :]).item()
         lower_loss = F.mse_loss(envmap_train[:, H_half:, :], self.gt_envmap[:, :H_half, :]).item()
