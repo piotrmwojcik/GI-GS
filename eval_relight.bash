4969d49a6d5050bf10bc9a95746cb3da5a60cfde
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-11 18:15:20 +0200

    albedo and relight

--- eval_relight.bash
+++ eval_relight.bash
@@ -19,7 +19,8 @@ for d in "${datasets[@]}"; do
     export DATA_SUBDIR
     echo "Processing DATASET: $d with DATA_SUBDIR: $DATA_SUBDIR"
 
-    python render.py -m outputs/$d -s data/$d --checkpoint outputs/$d/$DATA_SUBDIR/chkpnt35000.pth --eval --skip_train --brdf_eval
-
+python relight.py -m outputs/$d -s data/$d \
+      --checkpoint outputs/$d/$DATA_SUBDIR/chkpnt35000.pth --hdri data/$d/$DATA_SUBDIR.hdr \
+      --eval --gamma
   done
 done
