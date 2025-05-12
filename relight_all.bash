a0f34e37ab1ba581efa5371df7a6844a0faef451
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-12 14:41:32 +0200

    relight all

--- relight_all.bash
+++ relight_all.bash
@@ -11,23 +11,24 @@ datasets=(
 
 # Loop through each dataset and call the training script
 for d in "${datasets[@]}"; do
+  pairs=(
+    "chapel_day_4k_32x16_rot0 golden_bay_4k_32x16_rot330"
+    "dam_wall_4k_32x16_rot90 small_harbour_sunset_4k_32x16_rot270"
+    "golden_bay_4k_32x16_rot330 dam_wall_4k_32x16_rot90"
+  )
+
   for pair in "${pairs[@]}"; do
-    pairs=(
-      "chapel_day_4k_32x16_rot0 golden_bay_4k_32x16_rot330"
-      "dam_wall_4k_32x16_rot90 small_harbour_sunset_4k_32x16_rot270"
-      "golden_bay_4k_32x16_rot330 dam_wall_4k_32x16_rot90"
-    )
     read DATA_SUBDIR MAP_NAME <<< "$pair"
     export DATA_SUBDIR
-    echo "Processing DATASET: $d with DATA_SUBDIR: $DATA_SUBDIR and MAP_NAME $MAP_NAME"
+    echo "Processing DATASET: $d with DATA_SUBDIR: $DATA_SUBDIR and MAP_NAME: $MAP_NAME"
 
-    #python relight.py \
-    #  -m outputs/bicycle \
-    #  -s datasets/nerf_real_360/bicycle/ \
-    #  --checkpoint outputs/bicycle/chkpnt40000.pth \
-    #  --hdri datasets/TensoIR/Environment_Maps/high_res_envmaps_2k/bridge.hdr \
-    #  --eval \
-    #  --gamma
-    done
+    # Uncomment below to execute your actual command
+    # python relight.py \
+    #   -m outputs/"$d" \
+    #   -s datasets/nerf_real_360/"$d"/ \
+    #   --checkpoint outputs/"$d"/chkpnt40000.pth \
+    #   --hdri datasets/TensoIR/Environment_Maps/high_res_envmaps_2k/"$MAP_NAME".hdr \
+    #   --eval \
+    #   --gamma
   done
 done
