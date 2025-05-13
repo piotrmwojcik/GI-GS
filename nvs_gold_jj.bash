ea2a699505f8ecae897251869ca4db1c2c715faa
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-13 02:03:11 +0200

    alpha mask

--- nvs_gold_jj.bash
+++ nvs_gold_jj.bash
@@ -15,7 +15,7 @@ for d in "${datasets[@]}"; do
     echo "Processing DATASET: $d with DATA_SUBDIR: $DATA_SUBDIR"
 
     python render.py \
-    -m outputs/"$d"/$DATA_SUBDIR \
+    -m outputs_diffuse/"$d"/$DATA_SUBDIR \
     -s data/"$d" \
     --checkpoint outputs/$DATA_SUBDIR/chkpnt35000.pth \
     --eval \
