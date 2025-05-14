30df3aa360d7c401921a43832fefdc445aa1b4de
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-14 15:42:42 +0200

    add script to collec

--- nvs_all.bash
+++ nvs_all.bash
@@ -2,11 +2,11 @@
 
 # List of dataset names
 datasets=(
-    hook150_v3_transl_statictimestep1
-    jumpingjacks150_v3_tex_statictimestep75
-    mouse150_v2_transl_statictimestep1
+    #hook150_v3_transl_statictimestep1
+    #jumpingjacks150_v3_tex_statictimestep75
+    #mouse150_v2_transl_statictimestep1
     spheres_cube_dataset_v5_statictimestep1
-    standup150_v3_statictimestep75
+    #standup150_v3_statictimestep75
 )
 
 # Loop through each dataset and call the training script
