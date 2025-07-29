4d20ad0b809c3d5f11ca4168ebeae2bfaa8d53f0
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-07-29 12:23:55 +0200

    do appendix

---
+++ nvs_all_v5.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# List of dataset names
+datasets=(
+    hook150_v5_spec32_statictimestep1
+    jumpingjacks150_v5_spec32_statictimestep75
+    mouse150_v5_spec32_statictimestep1
+    spheres_v5_spec32_statictimestep1
+    standup150_v5_spec32_statictimestep75
+)
+
+# Loop through each dataset and call the training script
+for d in "${datasets[@]}"; do
+  for DATA_SUBDIR in \
+    chapel_day_4k_32x16_rot0 \
+    dam_wall_4k_32x16_rot90 \
+    golden_bay_4k_32x16_rot330
+  do
+    export DATA_SUBDIR
+    echo "Processing DATASET: $d with DATA_SUBDIR: $DATA_SUBDIR"
+
+    python render.py \
+    -m outputs_diffuse_appendix/"$d"/$DATA_SUBDIR \
+    -s data/"$d" \
+    --checkpoint outputs_diffuse_appendix/"$d"/$DATA_SUBDIR/chkpnt35000.pth \
+    --eval \
+    --skip_train \
+    --pbr \
+    --gamma \
+    --indirect
+  done
+done
